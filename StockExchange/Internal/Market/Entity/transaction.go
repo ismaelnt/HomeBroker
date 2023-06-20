fdf21e0d466ff432f3cda21c18cef1ef61f33755
Ismael Neto <ismaelnt97@gmail.com>
2023-06-20 08:47:15 -0300

    feat: add book entity

--- StockExchange/Internal/Market/Entity/transaction.go
+++ StockExchange/Internal/Market/Entity/transaction.go
@@ -28,3 +28,27 @@ func NewTransaction(sellingOrder *Order, buyingOrder *Order, shares int, price f
 		DateTime:     time.Now(),
 	}
 }
+
+func (t *Transaction) CalculateTotal(shares int, price float64) {
+	t.TotalPrice = float64(shares) * price
+}
+
+func (t *Transaction) CloseBuyOrder() {
+	if t.BuyingOrder.PendingShares == 0 {
+		t.BuyingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) CloseSellOrder() {
+	if t.SellingOrder.PendingShares == 0 {
+		t.SellingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) AddBuyOrderPendingShares(shares int) {
+	t.BuyingOrder.PendingShares += shares
+}
+
+func (t *Transaction) AddSellOrderPendingShares(shares int) {
+	t.SellingOrder.PendingShares += shares
+}
